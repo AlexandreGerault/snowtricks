6b94f7ad46b62ecd1647333e49f613eff0b673cb
Alexandre Gérault <alexandre@hexium.io>
2021-08-08 15:33:11 +0200

    feat(login): implement infrastructure

---
+++ apps/symfony/src/Infrastructure/Security/Guard/AuthenticationChecker.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Infrastructure\Security\Guard;
+
+use App\Infrastructure\Security\Contracts\Repository\MembersRepositoryInterface;
+use Domain\Security\Entity\Member;
+use Domain\Security\Providers\AuthProviderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AuthenticationChecker implements AuthProviderInterface
+{
+    public function __construct(
+        private MembersRepositoryInterface $repository,
+        private UserPasswordHasherInterface $hasher
+    ) {
+    }
+
+    public function check(Member $member, string $plainPassword): bool
+    {
+        $user = $this->repository->getUserByEmail($member->email());
+
+        return $this->hasher->isPasswordValid($user, $plainPassword);
+    }
+}
---
+++ apps/symfony/src/Infrastructure/Security/Guard/LoginFormAuthenticator.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Security\Guard;
+
+use App\Infrastructure\Security\Contracts\Repository\MembersRepositoryInterface;
+use Domain\Security\Entity\Member;
+use Domain\Security\Providers\AuthProviderInterface;
+use Domain\Security\UseCases\Login\Login;
+use Domain\Security\UseCases\Login\LoginPresenterInterface;
+use Domain\Security\UseCases\Login\LoginRequest;
+use Domain\Security\UseCases\Login\LoginResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator implements LoginPresenterInterface
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    private LoginResponse $response;
+
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private Login $login
+    ) {
+    }
+
+    public function authenticate(Request $request): PassportInterface
+    {
+        $email = $request->request->get('email', '');
+        $password = $request->request->get('password', '');
+
+        $loginRequest = new LoginRequest($email, $password);
+        $this->login->execute($loginRequest, $this);
+
+        $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge('authenticate', $request->get('_csrf_token')),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        // For example:
+        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        if ($request->hasSession()) {
+            $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+            $request->getSession()->set('login_errors', $this->response->getErrors());
+        }
+
+        $url = $this->getLoginUrl($request);
+
+        return new RedirectResponse($url);
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+
+    public function presents(LoginResponse $response): void
+    {
+        $this->response = $response;
+    }
+}
